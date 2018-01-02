d85a2c16588ca9271ad1955da4b7d300ac02fd01
Rudy Macias <rudymacias@me.com>
2018-01-01 23:35:08 -0600

    Committing course assignments

---
+++ 00-Getting-Started/README.md
@@ -0,0 +1,3 @@
+# Getting Started with PHP
+
+The first assignment of this course doesn't require any downloadable files.
---
+++ 01-Variables-Conditionals/01-assignment.php
@@ -0,0 +1,49 @@
+<?php
+
+/*****************************************
+*************  Instructions  *************
+******************************************
+         Fill in your responses
+        to this assignment below.
+*****************************************/
+
+
+
+// 01. Create and use a string variable
+
+
+
+
+
+
+// 02. Create and use an integer variable
+
+
+
+
+
+// 03. Create and use a boolean variable
+
+
+
+
+
+// 04. Create and call one or more function(s)
+
+
+
+
+
+// 05. Basic conditional statement
+
+
+
+
+
+// 01. Echo to page
+
+
+
+
+
+?>
