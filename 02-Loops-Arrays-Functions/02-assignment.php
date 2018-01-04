2259de45d3a17d079fefc2930dd672bff2ffe67f
Rudy Macias <rudymacias@me.com>
2018-01-04 05:22:01 -0600

    module 2 assignment

---
+++ 02-Loops-Arrays-Functions/02-assignment.php
@@ -0,0 +1,48 @@
+<?php
+
+/*****************************************
+*************  Instructions  *************
+******************************************
+         Fill in your responses
+        to this assignment below.
+*****************************************/
+
+
+
+// 01. Create and Call at least one function.
+
+
+
+
+
+// 02. Create a For Loop
+
+
+
+
+
+// 03. Create a While Loop
+
+
+
+
+
+// 04. Create an Array
+
+
+
+
+
+// 05. Sort an Array
+
+
+
+
+
+// 01. Create a Switch Statement
+
+
+
+
+
+?>
